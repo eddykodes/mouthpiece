3b043ef1da5658bc71581452c3db512ecd33acf7
unknown <eddy@gocollo.com>
2021-02-10 11:27:21 -0500

    add Login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './app/Login';
 import Navbar from './app/Navbar'
 import Example from './pages/Example';
 import Index from './pages/Index';
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path='/example' component={Example} />
+        <Route exact path='/login' component={Login} />
         <Route exact path='/' component={Index} />
       </Switch>
     </Router>
